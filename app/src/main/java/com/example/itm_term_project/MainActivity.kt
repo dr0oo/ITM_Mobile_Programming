8e2baedaf015cc6168a21416053024598aba6e42
dr0oo <jeongdooroo2160@g.seoultech.ac.kr>
2021-11-24 02:36:49 +0900

    appbar: set color set items(pi: personal info, str: storage)

--- app/src/main/java/com/example/itm_term_project/MainActivity.kt
+++ app/src/main/java/com/example/itm_term_project/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.itm_term_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.pi -> {
+            // User chose the "Settings" item, show the app settings UI...
+            true
+        }
+
+        R.id.str -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
 }

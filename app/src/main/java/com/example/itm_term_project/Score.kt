128acfdf5d4621dc148cab2732cb9b40dcdcd5d8
dr0oo <jeongdooroo2160@g.seoultech.ac.kr>
2021-12-07 23:32:53 +0900

    draw quarter note: - by Paint class - set background image as empty score
    good

--- app/src/main/java/com/example/itm_term_project/Score.kt
+++ app/src/main/java/com/example/itm_term_project/Score.kt
@@ -1,24 +1,42 @@
 package com.example.itm_term_project
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.ImageView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-class Score : AppCompatActivity() {
+class Score: AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.score)
+//        setContentView(R.layout.score)
 
-
-        val fl : FrameLayout = findViewById(R.id.score_page)
-        val iv = ImageView(this)
-
-        iv.setImageDrawable(resources.getDrawable(R.drawable.quarter_note))
-
-        fl.addView(iv)
+        val quarter_note = MyQuarterNote(this)
+        setContentView(quarter_note)
 
     }
 
+    class MyQuarterNote(cont: Context): View(cont){
+        var paint = Paint()
+        var circleX: Float = 0f
+        var circleY: Float = 0f
+        var circleRadius = 15f
+
+        override fun onDraw(canvas: Canvas){
+            paint.setColor(Color.BLACK)
+
+//            setBackgroundColor(Color.YELLOW)
+            setBackgroundResource(R.drawable.score_background_test)
+
+
+            circleX = 80f
+            circleY = 80f
+            canvas.drawCircle(circleX, circleY, circleRadius, paint)
+
+        }
+    }
 }

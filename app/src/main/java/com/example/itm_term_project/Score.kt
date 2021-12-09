80392ac8aa7414917f8eedef9a0475cdd9467736
dr0oo <jeongdooroo2160@g.seoultech.ac.kr>
2021-12-09 12:27:51 +0900

    audio permission: - ask permission when first started - if denied, close application
    audio recorder:
    - start recording, when button clicked

--- app/src/main/java/com/example/itm_term_project/Score.kt
+++ app/src/main/java/com/example/itm_term_project/Score.kt
@@ -1,25 +1,52 @@
 package com.example.itm_term_project
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.View
+import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 
 class Score: AppCompatActivity()  {
+
+    //오디오권한
+    private val requiredPermissions = arrayOf(
+        android.Manifest.permission.RECORD_AUDIO
+    )
+
+    //오디오권한
+    companion object{
+        private const val REQUESTED_RECORD_AUDIO_PERMISSION = 201
+    }
+
+    private var recorder: MediaRecorder? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.score)
 
+        //앱 실행시 오디오 권한 요청(처음에만)
+        requestAudioPermission()
+
         val fl : FrameLayout = findViewById(R.id.score_page)
 
         val quarter_note = MyQuarterNote(this)
         fl.addView(quarter_note)
+
+        val recording_button: Button = findViewById(R.id.recording_button)
+
+        recording_button.setOnClickListener({
+            startRecording()
+        })
 
     }
 
@@ -71,5 +98,59 @@ class Score: AppCompatActivity()  {
             lineY = circleY
             canvas.drawLine(lineX, lineY, lineX, lineY+90, paint)
         }
+    }
+
+    //오디오 권한 요청
+    private fun requestAudioPermission(){
+        requestPermissions(requiredPermissions, REQUESTED_RECORD_AUDIO_PERMISSION)
+    }
+
+    //오디오 권한 요청
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        
+        val audioRecordPermissionGranted=
+            requestCode == REQUESTED_RECORD_AUDIO_PERMISSION &&
+                    grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+        
+        if(!audioRecordPermissionGranted){
+            finish()
+            //오디오 권한 거절시 앱 종료
+        }
+    }
+
+    private fun startRecording(){
+        Toast.makeText(this, "recording start", Toast.LENGTH_SHORT).show()
+        recorder = MediaRecorder().apply{
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)//저장경로 나중에 바꾸기
+            prepare()
+        }
+        recorder?.start()
+//        recordTimeTextView.startCountup()
+//        soundVisualizerView.startVisualizing(false)
+//        state = State.ON_RECORDING
+//        https://whyprogrammer.tistory.com/584
+    }
+
+    private val recordingFilePath: String by lazy{
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private fun stopRecording(){
+        recorder?.run{
+            stop()
+            release()
+        }
+        recorder = null
+//        suondVisualizerView.stopVisualizing()
+//        recordTimeTextView.stopCountup()
+//        state = State.AFTER_RECORDING
     }
 }

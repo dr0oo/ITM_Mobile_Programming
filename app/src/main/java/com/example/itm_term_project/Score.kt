2aeb277894f0f2a0646118744f565737718e5610
dr0oo <jeongdooroo2160@g.seoultech.ac.kr>
2021-12-11 21:40:33 +0900

    change recording button state: - change icon of ImageButton when state changed - start and stop recording when state changed
    (don't know how to play recorded audio)

--- app/src/main/java/com/example/itm_term_project/Score.kt
+++ app/src/main/java/com/example/itm_term_project/Score.kt
@@ -2,16 +2,17 @@ package com.example.itm_term_project
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import android.icu.text.AlphabeticIndex
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
@@ -30,6 +31,10 @@ class Score: AppCompatActivity()  {
 
     private var recorder: MediaRecorder? = null
 
+    private var state = State.BEFORE_RECORDING
+
+    private var player: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.score)
@@ -42,61 +47,46 @@ class Score: AppCompatActivity()  {
         val quarter_note = MyQuarterNote(this)
         fl.addView(quarter_note)
 
-        val recording_button: Button = findViewById(R.id.recording_button)
+        val recording_button: ImageButton = findViewById(R.id.recording_button)
+        recording_button.setImageResource(R.drawable.ic_baseline_fiber_manual_record_24   )
 
         recording_button.setOnClickListener({
-            startRecording()
+            when(state){
+                State.BEFORE_RECORDING ->{
+                    recording_button.setImageResource(R.drawable.ic_baseline_stop_24)
+                    startRecording()
+                }
+                State.ON_RECORDING->{
+                    recording_button.setImageResource(R.drawable.ic_baseline_stop_24)
+                    stopRecording()
+                }
+                State.AFTER_RECORDING ->{
+                    recording_button.setAndShowEnabled(false)
+                }
+            }
         })
 
     }
 
-    class MyQuarterNote(cont: Context): View(cont){
-        var paint = Paint()
-        var circleX: Float = 0f
-        var circleY: Float = 0f
-        var lineX: Float = 0f
-        var lineY: Float = 0f
-        var circleRadius = 15f
+    //ImageButton setEnabled(false)
+    fun Drawable.toGrayscale(): Drawable =
+        deepCopy().apply { setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN) }
 
-        override fun onDraw(canvas: Canvas){
-            paint.setColor(Color.BLACK)
-            paint.setStrokeWidth(8f)
+    fun Drawable.deepCopy(): Drawable =
+        constantState?.newDrawable()?.mutate() ?:
+        throw RuntimeException("Called on null Drawable!")
 
-            setBackgroundResource(R.drawable.score_background_test)
-
-            circleX = 120f
-            circleY = 200f
-
-            canvas.drawCircle(circleX, circleY, circleRadius, paint)
-            if(checkLine(circleY).equals("left")){
-                leftLine(canvas, circleX, circleY)
-            }else{
-                rightLine(canvas, circleX, circleY)
-            }
+    fun ImageButton.setAndShowEnabled(enabled: Boolean) {
+        if (enabled == isEnabled)
+            return
+        isEnabled = enabled
+        if (enabled) {
+            setImageDrawable(tag as Drawable)
         }
-
-        //선을 왼쪽에 그릴건지 오른쪽에 그릴건지 리턴해줌
-        //나중에 이거는 음표 클래스에 옮겨도 될듯
-        fun checkLine(y: Float): String{
-            var where=""
-            when(y){
-                in 0f..155f -> where = "left"
-                in 156f..410f -> where = "right"
-                else -> where="I don't know"
-            }
-            return where
-        }
-
-        fun rightLine(canvas: Canvas, circleX: Float, circleY: Float){
-            lineX = circleX+15f
-            lineY = circleY
-            canvas.drawLine(lineX, lineY, lineX, lineY-90, paint)
-        }
-
-        fun leftLine(canvas: Canvas, circleX: Float, circleY: Float){
-            lineX = circleX-15f
-            lineY = circleY
-            canvas.drawLine(lineX, lineY, lineX, lineY+90, paint)
+        else {
+            if (tag == null)
+                tag = drawable
+            setImageDrawable(drawable.toGrayscale())
         }
     }
 
@@ -135,7 +125,7 @@ class Score: AppCompatActivity()  {
         recorder?.start()
 //        recordTimeTextView.startCountup()
 //        soundVisualizerView.startVisualizing(false)
-//        state = State.ON_RECORDING
+        state = State.ON_RECORDING
 //        https://whyprogrammer.tistory.com/584
     }
 
@@ -151,6 +141,37 @@ class Score: AppCompatActivity()  {
         recorder = null
 //        suondVisualizerView.stopVisualizing()
 //        recordTimeTextView.stopCountup()
-//        state = State.AFTER_RECORDING
+        state = State.AFTER_RECORDING
+    }
+
+
+    private fun startPlaying() {
+        // MediaPlayer
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                prepare() // 재생 할 수 있는 상태 (큰 파일 또는 네트워크로 가져올 때는 prepareAsync() )
+            }
+
+        // 전부 재생 했을 때
+        player?.setOnCompletionListener {
+            stopPlaying()
+            state = State.AFTER_RECORDING
+        }
+
+        player?.start() // 재생
+//        recordTimeTextView.startCountup()
+//        soundVisualizerView.startVisualizing(true)
+
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+//        soundVisualizerView.stopVisualizing()
+//        recordTimeTextView.stopCountup()
+
+        state = State.AFTER_RECORDING
     }
 }
